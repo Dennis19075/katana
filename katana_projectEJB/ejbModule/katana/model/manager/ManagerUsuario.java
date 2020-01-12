16672e7ddcedd86933d9d37ae83f03ae581942bb
Saito Inosaka <saitoinosaka@gmail.com>
2020-01-12 03:44:30 -0500

    Se esta intentando hacer el crud de Usuarios + UsuarioRol

--- katana_projectEJB/ejbModule/katana/model/manager/ManagerUsuario.java
+++ katana_projectEJB/ejbModule/katana/model/manager/ManagerUsuario.java
@@ -1,5 +1,8 @@
 package katana.model.manager;
 
+
+import katana.model.entities.UsuUsuarioRol;
+import katana.model.entities.ProTalla;
 import katana.model.entities.UsuRol;
 import katana.model.entities.UsuUsuario;
 
@@ -42,7 +45,12 @@ public class ManagerUsuario {
     public void insertarUsuario(UsuUsuario usuario) throws Exception {
     	if(findUsuarioByCorreo(usuario.getCorreo())!=null)
     		throw new Exception("Ya existe el correo ingresado");
-        em.persist(usuario);
+    	UsuUsuario aux = new UsuUsuario();
+    	aux.setNombre(usuario.getNombre());
+    	aux.setApellido(usuario.getApellido());
+    	aux.setCorreo(usuario.getCorreo());
+    	aux.setPassword(usuario.getPassword());
+        em.persist(aux);
     }
     public void eliminarUsuario(String correo) {
     	UsuUsuario usuario=findUsuarioByCorreo(correo);
@@ -54,11 +62,47 @@ public class ManagerUsuario {
     	if(e==null)
     		throw new Exception("No existe el usuario con este correo");
     	e.setApellido(usuario.getApellido());
+    	/*el correo no se si tambien se puede actualizar o no*/
     	e.setNombre(usuario.getNombre());
     	e.setPassword(usuario.getPassword());
     	em.merge(e);
     	
     }
     
+    
+    /**CRUD DE LA TALBLA usu_usuario_rol*/
+    public List<UsuUsuarioRol> findAllUsuariosRol(){
+    	String consulta="SELECT u FROM UsuUsuarioRol u";
+    	Query q=em.createQuery(consulta, UsuUsuarioRol.class);
+    	return q.getResultList();
+    }
+    
+    public UsuUsuarioRol findUsuarioRolById(int id) {
+    	return em.find(UsuUsuarioRol.class, id);
+    }
+    /*El metodo de insertar se ocupa dentro del metodo de insertarUsuario
+     * los demas metodos se van a poder editar en su propia vista*/
+    public void insertarUsuarioRol(UsuUsuario usuario, int id) throws Exception {
+    	UsuUsuarioRol aux=new UsuUsuarioRol();
+    	UsuRol rolAux = new UsuRol();
+    	aux.setUsuUsuario(usuario);
+    	rolAux.setIdRol(id); //aqui le cambio segun de que pagina venga los privilegios
+    	aux.setUsuRol(rolAux); //aqui ya guarda el rol segun lo que yo puse
+        em.persist(aux);
+    }
+    public void eliminarUsuarioRol(UsuUsuarioRol usuarioRol) {
+    	UsuUsuarioRol aux =findUsuarioRolById(usuarioRol.getIdUsuarioRol());
+    	if(aux!=null)
+    		em.remove(aux);
+    }
+    public void actualizarUsuarioRol(UsuUsuarioRol usuarioRol) throws Exception {
+    	UsuUsuarioRol e=findUsuarioRolById(usuarioRol.getIdUsuarioRol());
+    	if(e==null)
+    		throw new Exception("No existe el usuarioRol especificado.");
+    	e.setUsuUsuario(usuarioRol.getUsuUsuario());
+    	e.setUsuRol(usuarioRol.getUsuRol());
+    	em.merge(e);
+    	
+    }
     
 }
--- katana_projectWeb/src/katana/controller/BeanUsuario.java
+++ katana_projectWeb/src/katana/controller/BeanUsuario.java
@@ -5,10 +5,8 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 import katana.controller.JSFUtil;
-import katana.model.entities.ProColor;
-import katana.model.entities.ProTalla;
-import katana.model.entities.ProTipoProducto;
-import katana.model.manager.ManagerProducto;
+import katana.model.entities.UsuUsuario;
+import katana.model.manager.ManagerUsuario;
 
 import java.io.Serializable;
 import java.util.List;
@@ -18,77 +16,91 @@ import java.util.List;
 public class BeanUsuario implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@EJB
-	private ManagerProducto managerProducto;
-	private List<ProTipoProducto> listaTipoProducto;
-	private ProTipoProducto tipo_producto;
-	private boolean panelColapsado;
-	private ProTipoProducto tipo_productoSeleccionado;
+	private ManagerUsuario managerUsuario;
+
+	private List<UsuUsuario> listaUsuario;
+
+	private UsuUsuario usuario;
+
+	private boolean panelColapsado_usuario;
+
+	private UsuUsuario usuarioSeleccionado;
 	@PostConstruct
 	public void inicializar() 
 	{
-	    listaTipoProducto=managerProducto.findAllTipoProducto();
-	    tipo_producto=new ProTipoProducto();
-	    panelColapsado=true;
-	}
+	    listaUsuario=managerUsuario.findAllUsuarios();
+	    usuario=new UsuUsuario();
+	    panelColapsado_usuario=true;
+	}	
 	
-	/*BEAN PARA pro_tipo_producto*/
+/*BEAN PARA usu_usuario*/
 	
-	public void actionListenerColapsarPanel() {
-		panelColapsado=!panelColapsado;
+	public void actionListenerColapsarPanel_usuario() {
+		panelColapsado_usuario=!panelColapsado_usuario;
 	}
-	public void actionListenerInsertarTipoProducto() {
+	public void actionListenerInsertarUsuario() {
 		try {
-			managerProducto.insertarTipoProducto(tipo_producto);
-			listaTipoProducto=managerProducto.findAllTipoProducto();
-			tipo_producto=new ProTipoProducto();
-			JSFUtil.crearMensajeInfo("Se ha insertado el Tipo de Producto");
+			managerUsuario.insertarUsuario(usuario);
+			managerUsuario.insertarUsuarioRol(usuario, 5); //aqui le mando desde el xhtml el id por defecto
+			listaUsuario=managerUsuario.findAllUsuarios();
+			usuario = new UsuUsuario();
+			JSFUtil.crearMensajeInfo("Se ha insertado el usuario.");
 		} catch (Exception e) {
 			JSFUtil.crearMensajeError(e.getMessage());
 			e.printStackTrace();
 		}
 	}
-	public void actionListenerSeleccionarTipoProducto(ProTipoProducto tipoProducto) {
-		tipo_productoSeleccionado=tipoProducto;
+	public void actionListenerSeleccionarUsuario(UsuUsuario usuario) {
+		usuarioSeleccionado=usuario;
 	}
-	public void actionListenerActualizarTipoProducto() {
+	public void actionListenerActualizarUsuario() {
 		try {
-			managerProducto.actualizarTipoProducto(tipo_productoSeleccionado);
-			listaTipoProducto=managerProducto.findAllTipoProducto();
-			JSFUtil.crearMensajeInfo("Datos actualizados");
+			managerUsuario.actualizarUsuario(usuario);
+			listaUsuario=managerUsuario.findAllUsuarios();
+			JSFUtil.crearMensajeInfo("Datos actualizados.");
 		} catch (Exception e) {
 			JSFUtil.crearMensajeError(e.getMessage());
 			e.printStackTrace();
 		}
 	}
-	public void actionListenerEliminarTipoProducto(int id) {
-		managerProducto.eliminarTipoProducto(id);
-		listaTipoProducto=managerProducto.findAllTipoProducto();
-		JSFUtil.crearMensajeInfo("Tipo de Producto eliminado");
+	public void actionListenerEliminarUsuario(String correo) {
+		managerUsuario.eliminarUsuario(correo);
+		listaUsuario=managerUsuario.findAllUsuarios();
+		JSFUtil.crearMensajeInfo("Usuario eliminado.");
 	}
-	public List<ProTipoProducto> getListaTipoProducto() {
-		return listaTipoProducto;
+
+	public List<UsuUsuario> getListaUsuario() {
+		return listaUsuario;
 	}
-	public void setListaTipoProducto(List<ProTipoProducto> listaTipoProducto) {
-		this.listaTipoProducto = listaTipoProducto;
+
+	public void setListaUsuario(List<UsuUsuario> listaUsuario) {
+		this.listaUsuario = listaUsuario;
 	}
-	public ProTipoProducto getTipo_producto() {
-		return tipo_producto;
+
+	public UsuUsuario getUsuario() {
+		return usuario;
 	}
-	public void setTipo_producto(ProTipoProducto tipo_producto) {
-		this.tipo_producto = tipo_producto;
+
+	public void setUsuario(UsuUsuario usuario) {
+		this.usuario = usuario;
 	}
-	public boolean isPanelColapsado() {
-		return panelColapsado;
+
+	public boolean isPanelColapsado_usuario() {
+		return panelColapsado_usuario;
 	}
-	public void setPanelColapsado(boolean panelColapsado) {
-		this.panelColapsado = panelColapsado;
+
+	public void setPanelColapsado_usuario(boolean panelColapsado_usuario) {
+		this.panelColapsado_usuario = panelColapsado_usuario;
 	}
-	public ProTipoProducto getTipo_productoSeleccionado() {
-		return tipo_productoSeleccionado;
+
+	public UsuUsuario getUsuarioSeleccionado() {
+		return usuarioSeleccionado;
 	}
-	public void setTipo_productoSeleccionado(ProTipoProducto tipo_productoSeleccionado) {
-		this.tipo_productoSeleccionado = tipo_productoSeleccionado;
+
+	public void setUsuarioSeleccionado(UsuUsuario usuarioSeleccionado) {
+		this.usuarioSeleccionado = usuarioSeleccionado;
 	}
+
 	
 	
 
